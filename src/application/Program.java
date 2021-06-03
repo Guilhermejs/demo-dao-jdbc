04eec2508047418f4261d72593a5dd918f12ccc4
Guilhermejs <guilherme.jacques@hotmail.com>
2021-06-03 00:48:58 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,8 @@ public class Program {
 		System.out.println(department);
 		
 		Seller seller = new Seller(1, "Guilherme", "guilherme@gmail.com", sdf.parse("02/04/1993"), 2000.0, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
